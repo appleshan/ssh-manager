258b9d5b0d0e694a8cb8acb4270ec8fd12e8f62c
Vivien Frenot <vivien.frenot@e5group.fr>
2017-05-10 14:56:36 +0200

    Rearange functions

--- ssh-manager.sh
+++ ssh-manager.sh
@@ -32,6 +32,70 @@ USE_IDN2=false
 
 #================== Functions ================================================
 
+function cecho() {
+	one_line=false
+	while [ "${1}" ]; do
+		case "${1}" in
+			-normal)        color="\033[00m" ;;
+			-black)         color="\033[30;01m" ;;
+			-red)           color="\033[31;01m" ;;
+			-green)         color="\033[32;01m" ;;
+			-yellow)        color="\033[33;01m" ;;
+			-blue)          color="\033[34;01m" ;;
+			-magenta)       color="\033[35;01m" ;;
+			-cyan)          color="\033[36;01m" ;;
+			-white)         color="\033[37;01m" ;;
+			-n)             one_line=true;  shift ; continue ;;
+			*)              echo -n "${1}"; shift ; continue ;;
+		esac
+		shift
+		if ${ENABLE_CECHO}; then
+			echo -en "${color}"
+		fi
+		echo -en "${1}"
+		if ${ENABLE_CECHO}; then
+			echo -en "\033[00m"
+		fi
+		shift
+	done
+	if ! ${one_line}; then
+		echo
+	fi
+}
+
+function probe ()
+{
+	als=$1
+	awk=$(awk -F "$DATA_DELIM" -v als="$als" '$1 == als { print $0 }' $HOST_FILE 2> /dev/null)
+	if [[ -z "$awk" ]]; then
+		return 1;
+	fi
+}
+
+function get_raw ()
+{
+	als=$1
+	awk -F "$DATA_DELIM" -v als="$als" '$1 == als { print $0 }' $HOST_FILE 2> /dev/null
+}
+
+function get_addr ()
+{
+	als=$1
+	get_raw "$als" | awk -F "$DATA_DELIM" '{ print $'$DATA_HADDR' }'
+}
+
+function get_port ()
+{
+	als=$1
+	get_raw "$als" | awk -F "$DATA_DELIM" '{ print $'$DATA_HPORT'}'
+}
+
+function get_user ()
+{
+	als=$1
+	get_raw "$als" | awk -F "$DATA_DELIM" '{ print $'$DATA_HUSER' }'
+}
+
 function exec_ping() {
 	_addr=$@
 	if ${USE_IDN2}; then
@@ -87,39 +151,6 @@ function list_commands() {
 	echo -ne "$0 "; cecho -n -yellow "export"; echo -ne "\t\t\t\t\t"; cecho -white "export config"
 }
 
-function probe ()
-{
-	als=$1
-	awk=$(awk -F "$DATA_DELIM" -v als="$als" '$1 == als { print $0 }' $HOST_FILE 2> /dev/null)
-	if [[ -z "$awk" ]]; then
-		return 1;
-	fi
-}
-
-function get_raw ()
-{
-	als=$1
-	awk -F "$DATA_DELIM" -v als="$als" '$1 == als { print $0 }' $HOST_FILE 2> /dev/null
-}
-
-function get_addr ()
-{
-	als=$1
-	get_raw "$als" | awk -F "$DATA_DELIM" '{ print $'$DATA_HADDR' }'
-}
-
-function get_port ()
-{
-	als=$1
-	get_raw "$als" | awk -F "$DATA_DELIM" '{ print $'$DATA_HPORT'}'
-}
-
-function get_user ()
-{
-	als=$1
-	get_raw "$als" | awk -F "$DATA_DELIM" '{ print $'$DATA_HUSER' }'
-}
-
 function server_add() {
 	# if user@host
 	# This grep syntaxt SHOULD be POSIX BRE compliant
@@ -151,45 +182,15 @@ function server_add() {
 	fi
 }
 
-function cecho() {
-	one_line=false
-	while [ "${1}" ]; do
-		case "${1}" in
-			-normal)        color="\033[00m" ;;
-			-black)         color="\033[30;01m" ;;
-			-red)           color="\033[31;01m" ;;
-			-green)         color="\033[32;01m" ;;
-			-yellow)        color="\033[33;01m" ;;
-			-blue)          color="\033[34;01m" ;;
-			-magenta)       color="\033[35;01m" ;;
-			-cyan)          color="\033[36;01m" ;;
-			-white)         color="\033[37;01m" ;;
-			-n)             one_line=true;  shift ; continue ;;
-			*)              echo -n "${1}"; shift ; continue ;;
-		esac
-		shift
-		if ${ENABLE_CECHO}; then
-			echo -en "${color}"
-		fi
-		echo -en "${1}"
-		if ${ENABLE_CECHO}; then
-			echo -en "\033[00m"
-		fi
-		shift
-	done
-	if ! ${one_line}; then
-		echo
-	fi
-}
-
 #=============================================================================
 
 cmd=$1
 alias=$2
 user=$3
 
-# if host file doesn't exist
+# if config directory doesn't exist
 if [ ! -d $CONF_DIR ]; then mkdir "$CONF_DIR"; fi
+# if host file doesn't exist
 if [ ! -f $HOST_FILE ]; then
 	if [ -f "$HOME/.ssh_servers" ]; then
 		mv "$HOME/.ssh_servers" $HOST_FILE
@@ -197,6 +198,7 @@ if [ ! -f $HOST_FILE ]; then
 		touch "$HOST_FILE"
 	fi
 fi
+# if config file doesn't exist
 if [ ! -f $CONF_FILE ]; then touch "$CONF_FILE"; fi
 
 # without args
